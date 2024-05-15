628bd95eda325a887ae6dbad9833f8edec30593d
mentrinusantara <ferryyoyo@gmail.com>
2024-05-15 16:11:50 +0700

    Initial Commit

---
+++ Aike/AikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AikeApp.swift
+//  Aike
+//
+//  Created by Ferry Setiawan on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aike
+//
+//  Created by Ferry Setiawan on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
